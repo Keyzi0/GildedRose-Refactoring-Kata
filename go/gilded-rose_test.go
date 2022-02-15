f56c3b7630a85132be95a50eb728f33155dfa80b
degtyarev@rt-bpm.ru <degtyarev@rt-bpm.ru>
2022-02-15 23:58:30 +0400

    тесты для старого набора товаров

--- go/gilded-rose_test.go
+++ go/gilded-rose_test.go
@@ -1,15 +1,107 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func Test_Foo(t *testing.T) {
-	var items = []*Item{
-		&Item{"foo", 0, 0},
+	"gotest.tools/assert"
+)
+
+func Test_Aged_Brie(t *testing.T) {
+	name := "Aged Brie"
+
+	var updatedItems = []*Item{
+		{name, -1, 2},
+		{name, 4, 6},
+		{name, -11, 12},
+		{name, 9, 11},
+		{name, 14, 16},
+		{name, -1, -3},
+		{name, -1, 50},
+		{name, -1, 51},
 	}
+
+	doUpdateQuality(t, name, updatedItems)
+}
+
+func Test_Simple_Item(t *testing.T) {
+	name := "Simple Item"
+
+	var updatedItems = []*Item{
+		{name, -1, 0},
+		{name, 4, 4},
+		{name, -11, 8},
+		{name, 9, 9},
+		{name, 14, 14},
+		{name, -1, -5},
+		{name, -1, 47},
+		{name, -1, 49},
+	}
+
+	doUpdateQuality(t, name, updatedItems)
+}
+
+func Test_Sulfuras(t *testing.T) {
+	name := "Sulfuras, Hand of Ragnaros"
+
+	var updatedItems = []*Item{
+		{name, 0, 0},
+		{name, 5, 5},
+		{name, -10, 10},
+		{name, 10, 10},
+		{name, 15, 15},
+		{name, 0, -5},
+		{name, 0, 49},
+		{name, 0, 51},
+	}
+
+	doUpdateQuality(t, name, updatedItems)
+}
+
+func Test_Backstage_Passes(t *testing.T) {
+	name := "Backstage passes to a TAFKAL80ETC concert"
+
+	var updatedItems = []*Item{
+		{name, -1, 0},
+		{name, 4, 8},
+		{name, -11, 0},
+		{name, 9, 12},
+		{name, 14, 16},
+		{name, -1, 0},
+		{name, -1, 0},
+		{name, -1, 0},
+	}
+
+	doUpdateQuality(t, name, updatedItems)
+}
+
+// =========================================================
+func printItems(name string, items []*Item) {
+	fmt.Printf("\n%v\n", name)
+	for i, item := range items {
+		fmt.Println(fmt.Sprintf("%v. s:%v q:%v", i, item.SellIn, item.Quality))
+	}
+}
+
+func doUpdateQuality(t *testing.T, name string, expectedItems []*Item) {
+	var items = []*Item{
+		{name, 0, 0},
+		{name, 5, 5},
+		{name, -10, 10},
+		{name, 10, 10},
+		{name, 15, 15},
+		{name, 0, -5},
+		{name, 0, 49},
+		{name, 0, 51},
+	}
+
+	printItems(name, items)
 
 	UpdateQuality(items)
 
-	if items[0].name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].name)
+	printItems(name, items)
+
+	for i, item := range items {
+		assert.Equal(t, *item, *expectedItems[i])
 	}
 }
